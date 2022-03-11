9761c8c1a7bd945af654dd0706350e73a6b090fe
Geoff Sutcliffe <2geoff@pobox.com>
2022-03-11 14:43:01 -0500

    No diving

--- Diving/UMSCUBA/Links_files/Links.js
+++
@@ -1,7 +0,0 @@
-// Created by iWeb 3.0.4 local-build-20140919
-
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('Links_files/LinksMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id5');performPostEffectsFixups()}
