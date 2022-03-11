a358e88328e8fa87dbb5563726d44ceec1ed7a5d
Geoff Sutcliffe <2geoff@pobox.com>
2022-03-11 14:39:04 -0500

    Diving

---
+++ Diving/UMSCUBA/Contact_Us_files/Contact_Us.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20150120
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Contact_Us_files/Contact_UsMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');fixAllIEPNGs('Media/transparent.gif');IMpreload('Contact_Us_files','shapeimage_4','0');IMpreload('Contact_Us_files','shapeimage_5','0');IMpreload('Contact_Us_files','shapeimage_6','0');IMpreload('Contact_Us_files','shapeimage_7','0');IMpreload('Contact_Us_files','shapeimage_8','0');IMpreload('Contact_Us_files','shapeimage_9','0');IMpreload('Contact_Us_files','shapeimage_10','0');IMpreload('Contact_Us_files','shapeimage_11','0');IMpreload('Contact_Us_files','shapeimage_17','0');IMpreload('Contact_Us_files','shapeimage_18','0');fixupIECSS3Opacity('id4');applyEffects()}
